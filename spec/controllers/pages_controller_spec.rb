6f2f76ffa71a6615c04aecca7debbeb30c0966c7
israe eghribel <israe.eghribel@student.emi.ac.ma>
2013-01-31 17:41:00 +0000

    fini avec les pages statiques

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
-    it "should be successful" do
+    it "devrait reussir" do
       get 'home'
-      response.should be_success
+       response.should have_selector("title",
+                    :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
   describe "GET 'contact'" do
-    it "should be successful" do
+    it "devrait reussir" do
       get 'contact'
-      response.should be_success
+       response.should have_selector("title",
+                    :content => "Simple App du Tutoriel Ruby on Rails | Contact")
     end
   end
 
+  describe "GET 'about'" do
+    it "devrait reussir" do
+      get 'about'
+       response.should have_selector("title",
+                    :content => "Simple App du Tutoriel Ruby on Rails | A propos")
+    end
+  end
 end
